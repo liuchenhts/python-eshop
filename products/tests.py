bcbd984d5a922d6d55b55d9433509302b636d9bc
Chen Liu <liuchenhts@gmail.com>
2017-12-02 23:19:19 +1100

    add tests for business logic and web request

--- products/tests.py
+++ products/tests.py
@@ -1,7 +1,29 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from .models import Product
 
 # Create your tests here.
 class ProductModelTests(TestCase):
+
     def testProductCRUD(self):
         """
+            some comments
         """
+        product = Product()
+        product.name="test product"
+        product.price=1
+        product.save()
+
+        p = get_object_or_404(Product, pk=product.id)
+        print (p)
+        self.assertEqual(p.name, product.name)
+
+        url = reverse('products:read', kwargs={'pk': product.id})
+        response = self.client.get(url)
+        self.assertContains(response, p.name)
+
+
+        url = reverse('products:read', kwargs={'pk': 10000})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
