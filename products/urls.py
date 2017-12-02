2ea3909a67a300e769ce9a4846c5642da6353b38
Chen Liu <liuchenhts@gmail.com>
2017-12-02 20:32:03 +1100

    add bootstrap model for input button confirm

--- products/urls.py
+++ products/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^update/(?P<pk>[0-9]+)/$', views.update, name='update'),
     url(r'^update/$', views.update, name='update'),
     url(r'^delete/(?P<pk>[0-9]+)/$', views.delete, name='delete'),
+    url(r'^delete/$', views.delete, name='delete'),
 ]
